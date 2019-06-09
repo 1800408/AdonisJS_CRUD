18f3665620d16636c8521e5690299c4968be7012
1800408 <iw.bruno@live.com>
2019-06-09 15:20:06 -0300

    update

---
+++ app/Validators/CreateUser.js
@@ -0,0 +1,26 @@
+'use strict'
+//validador de usuario
+class CreateUser {
+  get rules () {
+    return {
+      'username': 'required|unique:users',
+      'email': 'required|unique:users',
+      'password':  'required'
+    }
+  }
+
+  get messages(){
+    return {
+      'required': 'Whoa now, {{ field}} is required.',
+      'unique': 'Wait a sec, the {{ field }} already exists.'
+    }
+  }
+  async fails(error){
+    this.ctx.session.withErrors(error)
+    .flashAll();
+
+    return this.ctx.response.redirect('back');
+  }
+}
+
+module.exports = CreateUser
---
+++ app/Validators/LoginUser.js
@@ -0,0 +1,27 @@
+'use strict'
+
+class LoginUser {
+  get rules () {
+    return {
+      // validation rules
+      'email': 'required|email',
+      'password': 'required'
+    }
+  }
+
+  get messages(){
+    return{
+      'required': 'Woah now, {{ field }} is required'
+    }
+  }
+
+  async fails(error){
+    this.ctx.session.withErrors(error)
+    .flashAll();
+
+    return this.ctx.response.redirect('back');
+  }
+
+}
+
+module.exports = LoginUser
